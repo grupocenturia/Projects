014d45e1fccaee4804c4727f6cc4c32fb45d521b
Rubén Villalobos <rvillalobos@grupocenturia.com>
2020-05-15 18:22:51 -0700

    vvvvvv

--- DevTest1/DevTest1/Controllers/HomeController.cs
+++ DevTest1/DevTest1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DevTest1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Prueba.";
+            ViewBag.Message = "Prue";
 
             return View();
         }
