aecc02ba0d1356a6685400e1ffbc101c2ee54a65
Ruben <rvillalobos@grupocenturia.com>
2020-12-08 16:05:19 -0600

    Actualizando la parte web

--- Centuria/CenturiaWeb/Controllers/HomeController.cs
+++ Centuria/CenturiaWeb/Controllers/HomeController.cs
@@ -5,26 +5,56 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace CenturiaWeb.Controllers
-{
+{ 
     public class HomeController : Controller
+{
+    public ActionResult Index()
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        return View();
     }
+
+    public ActionResult About()
+    {
+        ViewBag.Message = "Your application description page.";
+
+        return View();
+    }
+
+    public ActionResult Menu()
+    {
+        ViewBag.Message = "Your application description page.";
+
+        return View();
+    }
+
+    public ActionResult Administrator()
+    {
+        ViewBag.Message = "Administrator";
+
+        return View();
+    }
+
+    public ActionResult Language()
+    {
+        ViewBag.Message = "Language";
+
+        return View();
+    }
+
+    public ActionResult Profile()
+    {    
+        ViewBag.Message = "Perfil";
+       
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Contact(string User)
+    {
+        ViewBag.Message = "Your contact page." + User;
+
+        return View();
+    }
+
+}
 }
