2f8a038fb3a76c79acffd8de5f3e2983ecc1bb5c
Ruben <rvillalobos@grupocenturia.com>
2020-12-09 12:09:17 -0600

    creando los controladores y las vistas

---
+++ Centuria/CenturiaWeb/Controllers/AdministratorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CenturiaWeb.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        // GET: Administrator
+
+        public ActionResult Administrator()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult Language()
+        {
+            return View();
+        }
+    }
+}
---
+++ Centuria/CenturiaWeb/Controllers/CenturiaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CenturiaWeb.Controllers
+{
+    public class CenturiaController : Controller
+    {
+        // GET: Centuria
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+
+            return View();
+        }
+    }
+}
